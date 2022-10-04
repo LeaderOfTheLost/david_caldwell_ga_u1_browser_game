08837febe221098bfc778f743c3eb7fc073fa590
David Caldwell <doubledc92@gmail.com>
2022-10-04 10:14:27 -0400

    started app.js, working on getting pegs to move

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+// Global Variables
+let currentPlayer = 'red'
+let playing = false
+let diceRoll = Math.floor(Math.random() * 6 + 1)
+let diceBlock = document.querySelector('.dice')
+const blocks = document.querySelectorAll('.boardBlocks')
+const message = document.querySelector('.announcements')
+const playBtn = document.querySelector('#playBtn')
+const rollBtn = document.querySelector('#rollBtn')
 
+//game logic functions
+console.log(diceRoll)
+const startGame = () => {
+  message.textContent = `${currentPlayer.toUpperCase()}'s turn`
+  playing = true
+}
+startGame()
+
+const playGame = () => {
+  currentPlayer = 'red'
+  message.textContent = `${currentPlayer}'s turn`
+  diceBlock.textContent = '0'
+  playing = true
+}
+const rollDice = () => {
+  diceBlock.textContent = diceRoll
+}
+const playerMove = () => {}
+const winner = () => {}
+
+//event listeners
+playBtn.addEventListener('click', playGame)
+rollBtn.addEventListener('click', rollDice)
