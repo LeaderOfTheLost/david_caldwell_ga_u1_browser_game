8412dc9a11ac39441c8813ccf6469e7f12d44582
David Caldwell <doubledc92@gmail.com>
2022-10-04 11:53:57 -0400

    all buttons working, starting to move pegs

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // Global Variables
 let currentPlayer = 'red'
 let playing = false
-let diceRoll = Math.floor(Math.random() * 6 + 1)
+
 let diceBlock = document.querySelector('.dice')
 const blocks = document.querySelectorAll('.boardBlocks')
 const message = document.querySelector('.announcements')
 const playBtn = document.querySelector('#playBtn')
 const rollBtn = document.querySelector('#rollBtn')
+const bluePegs = document.querySelectorAll('.bluePegs')
+const redPegs = document.querySelectorAll('.redPegs')
+let blockSpace = blocks
 
 //game logic functions
-console.log(diceRoll)
+console.log(blockSpace)
 const startGame = () => {
   message.textContent = `${currentPlayer.toUpperCase()}'s turn`
   playing = true
@@ -18,14 +21,20 @@ startGame()
 
 const playGame = () => {
   currentPlayer = 'red'
-  message.textContent = `${currentPlayer}'s turn`
+  message.textContent = `${currentPlayer.toUpperCase()}'s turn`
   diceBlock.textContent = '0'
   playing = true
 }
 const rollDice = () => {
+  let diceRoll = Math.floor(Math.random() * 6 + 1)
   diceBlock.textContent = diceRoll
 }
-const playerMove = () => {}
+
+// move pegs
+const playerMove = () => {
+  for (let i = 0; i < bluePegs.length; i++) {}
+}
+
 const winner = () => {}
 
 //event listeners
