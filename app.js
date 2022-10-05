616960fecc977cc71f7e5746a6f72f707bfb97c2
David Caldwell <doubledc92@gmail.com>
2022-10-05 12:28:03 -0400

    pegs move to index of block

--- app.js
+++ app.js
@@ -16,16 +16,34 @@ const startGame = () => {
   playing = true
 }
 const rollDice = () => {
-  let diceRoll = Math.floor(Math.random() * 6 + 1)
-  diceBlock.textContent = diceRoll
+  let rollNum = Math.floor(Math.random() * 6 + 1)
+  return rollNum
 }
-
 // move pegs
 // add dice roll to index of blocks
 // move peg to that position
-const playerMove = () => {
-  let move = diceBlock.innerHTML
-  console.log(blocks)
+const redMove = () => {
+  let move = rollDice()
+  diceBlock.textContent = move
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (parseInt(blocks[i].getAttribute('blockIndex')) === move) {
+      blocks[move - 1].appendChild(redPeg)
+      let newPosition = parseInt(blocks[move].getAttribute('blockindex'))
+      console.log(newPosition)
+    }
+  }
+}
+
+const blueMove = () => {
+  let move = rollDice()
+  diceBlock.textContent = move
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (parseInt(blocks[i].getAttribute('blockindex')) === move) {
+      blocks[i - 1].appendChild(bluePeg)
+    }
+  }
 }
 const updateBlock = () => {}
 
@@ -42,5 +60,5 @@ startGame()
 //event listeners
 playBtn.addEventListener('click', startGame)
 rollBtn.addEventListener('click', rollDice)
-bluePeg.addEventListener('click', playerMove)
-redPeg.addEventListener('click', playerMove)
+bluePeg.addEventListener('click', blueMove)
+redPeg.addEventListener('click', redMove)
