0d09619c78ec813e674c39eb73547976fcc88eb4
David Caldwell <doubledc92@gmail.com>
2022-10-06 11:37:11 -0400

    game functions better, working on win alert

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Global Variables
 let currentPlayer = 'red'
 let playing = false
+let winner = false
 let diceBlock = document.querySelector('.dice')
 const blocks = document.querySelectorAll('.boardBlocks')
 const message = document.querySelector('.announcements')
@@ -11,9 +12,14 @@ const redPeg = document.querySelector('#redPeg')
 
 //game logic functions
 const startGame = () => {
+  let currentPlayer = 'red'
   message.textContent = `${currentPlayer.toUpperCase()}'s Rolls First`
   diceBlock.textContent = null
   playing = true
+  document.querySelector('#redStart').appendChild(redPeg)
+  document.querySelector('#blueStart').appendChild(bluePeg)
+  redCurrentPosition = 0
+  blueCurrentPosition = 0
 }
 const rollDice = () => {
   let rollNum = Math.floor(Math.random() * 6 + 1)
@@ -22,19 +28,37 @@ const rollDice = () => {
 // move pegs
 // add dice roll to index of blocks
 // move peg to that position
-let currentPosition = 0
-const playerMove = () => {
+let redCurrentPosition = 0
+const redMove = () => {
   let move = rollDice()
-
   diceBlock.textContent = move
-  blocks[move + currentPosition - 1].appendChild(redPeg)
-  currentPosition += parseInt(redPeg.parentNode.getAttribute('blockIndex'))
+  if (redCurrentPosition + move <= 15) {
+    blocks[move + redCurrentPosition - 1].appendChild(redPeg)
 
-  console.log(currentPosition)
-  if (currentPosition > 15) {
+    redCurrentPosition =
+      parseInt(redPeg.parentNode.getAttribute('blockIndex')) + 1
+  } else {
     document.querySelector('#redSafe').appendChild(redPeg)
   }
+  updateBlock()
+  switchPlayer()
+}
+let blueCurrentPosition = 0
+const blueMove = () => {
+  let move = rollDice()
+  diceBlock.textContent = move
+  if (blueCurrentPosition + move < 15) {
+    blocks[move + blueCurrentPosition - 1].appendChild(bluePeg)
+
+    blueCurrentPosition =
+      parseInt(bluePeg.parentNode.getAttribute('blockIndex')) + 1
+  } else {
+    document.querySelector('#blueSafe').appendChild(bluePeg)
+  }
   checkWinner()
+  updateBlock()
+
+  switchPlayer()
 }
 
 const switchPlayer = () => {
@@ -42,12 +66,24 @@ const switchPlayer = () => {
   message.textContent = `${currentPlayer.toUpperCase()}'s turn`
 }
 
+const updateBlock = () => {
+  console.log(redCurrentPosition)
+  if (currentPlayer === 'red' && redCurrentPosition === blueCurrentPosition) {
+    document.querySelector('#blueStart').appendChild(bluePeg)
+    blueCurrentPosition = 0
+  } else if (
+    currentPlayer === 'blue' &&
+    redCurrentPosition === blueCurrentPosition
+  ) {
+    document.querySelector('#redStart').appendChild(redPeg)
+    redCurrentPosition = 0
+  }
+}
 const checkWinner = () => {
-  let winner = false
-  if (document.querySelectorAll('.safe')[1].childNodes === redPeg) {
+  if (document.querySelector('#redSafe').children === redPeg) {
     winner = true
     alert('Red Wins')
-  } else if (document.querySelectorAll('.safe')[0].children === bluePeg) {
+  } else if (document.querySelector('#blueSafe').children === bluePeg) {
     winner = true
     alert('Blue Wins')
   }
@@ -57,5 +93,5 @@ startGame()
 //event listeners
 playBtn.addEventListener('click', startGame)
 rollBtn.addEventListener('click', rollDice)
-bluePeg.addEventListener('click', playerMove)
-redPeg.addEventListener('click', playerMove)
+bluePeg.addEventListener('click', blueMove)
+redPeg.addEventListener('click', redMove)
